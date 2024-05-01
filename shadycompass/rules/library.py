51b3207d42a23179e095d38f7e2c38a26bd3ce97
Patrick Double <pat@patdouble.com>
2024-04-30 21:34:17 -0500

    add services known to nmap
    Signed-off-by: Patrick Double <pat@patdouble.com>

--- shadycompass/rules/library.py
+++ shadycompass/rules/library.py
@@ -5,3 +5,7 @@ Constants for documentation links.
 METHOD_HTTP_BRUTE_FORCE = [
     'https://book.hacktricks.xyz/network-services-pentesting/pentesting-web#brute-force-directories-and-files',
 ]
+
+METHOD_HTTP_AUTOMATIC_SCANNERS = [
+    'https://book.hacktricks.xyz/network-services-pentesting/pentesting-web#automatic-scanners'
+]
