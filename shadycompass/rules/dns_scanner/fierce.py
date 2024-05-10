e67292b646d0181431f412e878db6f47f64d627b
Patrick Double <pat@patdouble.com>
2024-05-10 09:46:25 -0500

    add fierce dns tool
    Signed-off-by: Patrick Double <pat@patdouble.com>

--- shadycompass/rules/dns_scanner/fierce.py
+++ shadycompass/rules/dns_scanner/fierce.py
@@ -1,9 +1,12 @@
 from abc import ABC
+from math import floor
 
-from experta import DefFacts
+from experta import DefFacts, Rule, AS, NOT, MATCH
 
 from shadycompass import ToolAvailable
 from shadycompass.config import ToolCategory
+from shadycompass.facts import ScanNeeded, TargetDomain, RateLimitEnable
+from shadycompass.rules.conditions import TOOL_CONF, TOOL_PREF
 from shadycompass.rules.irules import IRules
 from shadycompass.rules.library import METHOD_DNS
 
@@ -23,4 +26,49 @@ class FierceRules(IRules, ABC):
             methodology_links=METHOD_DNS,
         )
 
-    # fierce --domain hospital.htb --dns-servers 10.129.229.189 | tee fierce-shadycompass.test.txt
+    def _declare_fierce(self, f1: ScanNeeded, domain: TargetDomain, ratelimit: RateLimitEnable = None):
+        addr = f1.get_addr()
+        addr_file_name_part = f'-{addr}-{f1.get_port()}'
+
+        more_options = []
+        if ratelimit:
+            more_options.append(['--delay', str(floor(60 / ratelimit.get_request_per_second()))])
+
+        command_line = self.resolve_command_line(
+            self.fierce_tool_name,
+            [
+                '--dns-servers', f'{f1.get_addr()}:{f1.get_port()}',
+            ], *more_options
+        )
+        command_line.extend([
+            '--domain', domain.get_domain(),
+            f'>fierce{addr_file_name_part}-{domain.get_domain()}.txt'
+        ])
+        self.recommend_tool(
+            category=ToolCategory.dns_scanner,
+            name=self.fierce_tool_name,
+            variant=domain.get_domain(),
+            command_line=command_line,
+            addr=f1.get_addr(),
+            port=f1.get_port(),
+        )
+
+    @Rule(
+        AS.f1 << ScanNeeded(category=ToolCategory.dns_scanner, addr=MATCH.addr),
+        AS.domain << TargetDomain(),
+        TOOL_PREF(ToolCategory.dns_scanner, fierce_tool_name),
+        TOOL_CONF(ToolCategory.dns_scanner, fierce_tool_name),
+        NOT(RateLimitEnable(addr=MATCH.addr)),
+    )
+    def run_fierce(self, f1: ScanNeeded, domain: TargetDomain):
+        self._declare_fierce(f1, domain)
+
+    @Rule(
+        AS.f1 << ScanNeeded(category=ToolCategory.dns_scanner, addr=MATCH.addr),
+        AS.domain << TargetDomain(),
+        AS.ratelimit << RateLimitEnable(addr=MATCH.addr),
+        TOOL_PREF(ToolCategory.dns_scanner, fierce_tool_name),
+        TOOL_CONF(ToolCategory.dns_scanner, fierce_tool_name),
+    )
+    def run_fierce_ratelimit(self, f1: ScanNeeded, domain: TargetDomain, ratelimit: RateLimitEnable):
+        self._declare_fierce(f1, domain, ratelimit=ratelimit)
